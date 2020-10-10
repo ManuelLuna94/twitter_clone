3ce75ed6d0a9b4b09241c352594fbc4be46475eb
Manuel Luna <manuellunada@gmail.com>
2020-10-10 17:10:26 -0500

    finish firts ui draft

---
+++ resources/views/timeline.blade.php
@@ -0,0 +1,15 @@
+<div class="border border-solid border-gray-500 rounded my-2 mx-4">
+    <form class="py-2 px-4">
+        <div class="flex px-10 py-4 items-center">
+            <img
+                src="https://static.wikia.nocookie.net/sailormooncrystal/images/1/19/Act23_89.jpg/revision/latest/top-crop/width/360/height/450?cb=20170722080641"
+                alt="user"
+                class="h-14 w-14 rounded-full"
+            >
+            <textarea class="w-full text-2xl text-gray-500 ml-4 bg-transparent" placeholder="What's happening?"></textarea>
+        </div>
+        <div class="flex justify-end">
+            <button class="text-white font-bold bg-blue-400 px-4 py-2 rounded-full mr-10">Tweet</button>
+        </div>
+    </form>
+</div>
